2b3000dde306224d67ec2d2b5e5fc3fce0b51966
Alexi Lyninger <alexi.lyninger@gmail.com>
2024-04-14 15:47:27 -0400

    added getByID and search functionality

--- index.js
+++ index.js
@@ -23,6 +23,52 @@ router.get('/', function (req, res, next) {
     });
 });
 
+//Create GET/search?Email=str&Name=str&Phone=n to search for contacts by Email and/or Name and/or Phone
+router.get('/search', function (req, res, next) {
+    let searchObject = {
+        "Email": req.query.Email,
+        "Name": req.query.Name,
+        "Phone": req.query.Phone
+    };
+
+    contactRepo.search(searchObject, function (data) {
+            res.status(200).json({
+                "status": 200,
+                "statusText": "OK",
+                "message": "All applicable contacts retrieved.",
+                "data": data
+            });
+    }, function(err) {
+        next(err);
+    });
+});
+
+//Create GET/Email to return a single contact
+router.get('/:Email', function (req, res, next) {
+    contactRepo.getByID(req.params.Email, function (data) {
+        if (data) {
+            res.status(200).json({
+                "status": 200,
+                "statusText": "OK",
+                "message": "All contact entries retrieved.",
+                "data": data
+            });
+        } else {
+            res.status(404).json({
+                "status": 404,
+                "statusText": "Not Found",
+                "message": "The conatact '" + req.params.Email + "' could not be found.",
+                "error": {
+                    "code": "NOT_FOUND",
+                    "message": "The conatact '" + req.params.Email + "' could not be found."
+                }
+            });
+        }
+    }, function(err) {
+        next(err);
+    });
+});
+
 //Configure router so all routes are prefixed with /api/v1
 app.use('/api/', router);
 
--- repos/contactRepo.js
+++ repos/contactRepo.js
@@ -12,7 +12,40 @@ let contactRepo = {
             resolve(JSON.parse(data));
         }
        });
-    }
+    },
+    getByID: function (Email, resolve, reject) {
+        fs.readFile(FILE_NAME, function (err, data) {
+            if (err) {
+                reject(err);
+            }
+            else {
+                let contacts = JSON.parse(data).find(c => c.Email === Email);
+                resolve(contacts);
+            }
+        });
+    },
+    search: function (searchObject, resolve, reject) {
+        fs.readFile(FILE_NAME, function (err, data) {
+            if (err) {
+                reject(err);
+            }
+            else {
+                let contacts = JSON.parse(data);
+                //Perform search
+                if (searchObject) {
+                    //Example search object
+                    //let searchObject = {
+                    //  "name": 'A'
+                    // };
+                    contacts = contacts.filter(
+                        c => (searchObject.Email ? c.Email.toLowerCase().indexOf(searchObject.Email.toLowerCase()) >= 0 : true) && 
+                        (searchObject.Name ? c.Name.toLowerCase().indexOf(searchObject.Name.toLowerCase()) >= 0 : true) &&
+                        (searchObject.Phone ? c.Phone.indexOf(searchObject.Phone) >= 0 : true));
+                }
+                resolve(contacts);
+            }
+        });
+    },
 };
 
 module.exports = contactRepo;
